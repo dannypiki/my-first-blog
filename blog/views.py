2afbe586033d4d22c5d4639fdf9b3fd4d7c7ad53
dannypiki <dannygarwe@gmail.com>
2018-12-23 16:26:40 +0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,7 @@ from django.shortcuts import render, get_object_or_404
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+from .forms import PostForm
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
